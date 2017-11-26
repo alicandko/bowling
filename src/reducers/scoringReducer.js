d86b1c34b1b6067d4be789dc0be09d34897abaa8
Alican Dedekarginoglu <alican.dedekarginoglu@pidoco.com>
2017-11-26 18:43:52 +0100

    Converted roll to a state

--- src/reducers/scoringReducer.js
+++ src/reducers/scoringReducer.js
@@ -2,49 +2,49 @@ import * as types from '../constants/actionTypes';
 import { getStandingPins, getDownPinsList, getScores, isFrameComplete, isGameComplete } from './scoringUtilities';
 import { List } from 'immutable';
 
-var roll = 0;
-
 const initialState = {
   standingPins: 10,
   downPinsList: List(),
   scores: List(),
+  roll: 0,
   frame: 1,
   frameComplete: false,
   gameComplete: false
 };
 
-export const scoringReducer = (state = initialState, action) => {
+const scoringReducer = (state = initialState, action) => {
   switch (action.type) {
   case types.ROLL: {
-    roll++;
-    const downPinsList = getDownPinsList(state.downPinsList, state.frame, roll, action.downPins);
+    const downPinsList = getDownPinsList(state.downPinsList, state.frame, state.roll, action.downPins);
     const scores = getScores(downPinsList, state.frame);
     return {
       standingPins: getStandingPins(state.standingPins, action.downPins),
       downPinsList,
       scores,
+      roll: state.roll + 1,
       frame: state.frame,
-      frameComplete: isFrameComplete(roll, action.downPins),
+      frameComplete: isFrameComplete(state.roll, action.downPins),
       gameComplete: isGameComplete(state.scores)  
     };
   }
 
   case types.NEW_FRAME: 
-    roll = 0;
     return {
       standingPins: 10,
       downPinsList: state.downPinsList,
       scores: getScores(state.downPinsList, state.frame),
+      roll: 0,
       frame: state.frame + 1,
       frameComplete: false,
       gameComplete: isGameComplete(state.scores)   
     };
 
   case types.NEW_GAME: 
-    roll = 0;
     return initialState;
     
   default:
     return state;
   }
 };
+
+export default scoringReducer;
