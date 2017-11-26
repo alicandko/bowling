f67d1479601f513a1352637c9066709b60393b26
Alican Dedekarginoglu <alican.dedekarginoglu@pidoco.com>
2017-11-26 02:56:00 +0100

    Implemented BowlingAlley component

--- src/components/BowlingAlley/BowlingAlley.js
+++ src/components/BowlingAlley/BowlingAlley.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './BowlingAlley.css';
 
-const BowlingAlley = ({ onThrowClick, onNewGameClick }) => (
-  <div>
-    <button type="button" onClick={onThrowClick}>Throw</button>
-    <button type="button" onClick={onNewGameClick}>New Game</button>
+const BowlingAlley = ({ pins, frameComplete, gameComplete, onThrowClick, onNewGameClick }) => (
+  <div className="bowling-alley-container">
+    <div className="bowling-alley">
+      <div className="pins">
+        <ul>
+          {pins.map(pinRow =>
+            <pre>
+              <li className='aa'>{ pinRow }</li>
+            </pre>
+          )}
+        </ul>
+      </div>
+      <div className="buttons">
+        <button disabled={isButtonDisabled(frameComplete, gameComplete)} onClick={onThrowClick}>Throw</button>
+        <button onClick={onNewGameClick}>New Game</button>
+      </div>
+    </div>
   </div>
 );
+
+const isButtonDisabled = (frameComplete, gameComplete) => {
+  return frameComplete || gameComplete;
+};
 
 export default BowlingAlley;
--- src/containers/BowlingAlleyContainer.js
+++ src/containers/BowlingAlleyContainer.js
@@ -1,6 +1,29 @@
 import { connect } from 'react-redux';
 import BowlingAlley from '../components/BowlingAlley/BowlingAlley';
 import { play, newGame } from '../actions';
+import { List } from 'immutable';
+
+const getPins = (standingPins) => {
+  let pins = List();  
+  let row = List.of(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+  const pinPositions = List.of([3, 1], [3, 3], [3, 5], [3, 7], [2, 2], [2, 4], [2, 6], [1, 3], [1,5], [0, 4]);
+  for (let i = 0; i < 4; i++) { 
+    pins = pins.push(row);
+  }
+
+  for (let i = 0; i < standingPins; i++) {
+    const y = (pinPositions.get(i)[0]);
+    const x = (pinPositions.get(i)[1]);
+    pins = pins.setIn([y, x], 'O');
+  }
+  return pins.reverse();
+};
+
+const mapStateToProps = (state) => ({
+  pins: getPins(state.scoringReducer.standingPins),
+  frameComplete: state.scoringReducer.frameComplete,
+  gameComplete: state.scoringReducer.gameComplete
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onThrowClick: () => {
@@ -12,7 +35,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const BowlingAlleyContainer = connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(BowlingAlley);
 
