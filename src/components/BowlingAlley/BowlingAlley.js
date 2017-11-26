1c708eac55de857c697fd2e763cf151d37b7bd2c
Alican Dedekarginoglu <alican.dedekarginoglu@pidoco.com>
2017-11-26 19:12:22 +0100

    Added prop types and key to components

--- src/components/BowlingAlley/BowlingAlley.js
+++ src/components/BowlingAlley/BowlingAlley.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { List } from 'immutable';
 import './BowlingAlley.css';
 
 const BowlingAlley = ({ pins, frameComplete, gameComplete, onThrowClick, onNewGameClick }) => (
@@ -6,8 +8,8 @@ const BowlingAlley = ({ pins, frameComplete, gameComplete, onThrowClick, onNewGa
     <div className="bowling-alley">
       <div className="pins">
         <ul>
-          {pins.map(pinRow =>
-            <pre>
+          {pins.map((pinRow, key) =>
+            <pre key={key}>
               <li className='aa'>{ pinRow }</li>
             </pre>
           )}
@@ -23,6 +25,14 @@ const BowlingAlley = ({ pins, frameComplete, gameComplete, onThrowClick, onNewGa
 
 const isButtonDisabled = (frameComplete, gameComplete) => {
   return frameComplete || gameComplete;
+};
+
+BowlingAlley.propTypes = {
+  pins: PropTypes.instanceOf(List).isRequired,
+  frameComplete: PropTypes.bool.isRequired,
+  gameComplete: PropTypes.bool.isRequired,
+  onThrowClick: PropTypes.func.isRequired,
+  onNewGameClick: PropTypes.func.isRequired
 };
 
 export default BowlingAlley;
--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
-//import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { List } from 'immutable';
 import './Scoreboard.css';
 
 const Scoreboard = ({ content }) => (
   <div className='scoreboard-container'>
     <div className='scoreboard'>
-      {content.map(frame => 
-        <div className='frame'>
+      {content.map((frame, key) => 
+        <div key={key} className='frame'>
           <div className='roll-container'>
             <div className='roll'>{ frame.get(0) }</div>
             <div className='roll'>{ frame.get(1) }</div>
@@ -17,5 +18,9 @@ const Scoreboard = ({ content }) => (
     </div>
   </div>
 );
+
+Scoreboard.propTypes = {
+  content: PropTypes.instanceOf(List).isRequired
+};
 
 export default Scoreboard;
